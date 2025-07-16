58bdbb8a29080d53bfcfeb6facc743fc709a9b82
mohit-singh-13 <mohitsingh092003@gmail.com>
2025-07-17 00:14:27 +0530

    App styling is done

---
+++ components/PrimaryButton.js
@@ -0,0 +1,47 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  function pressHandler() {}
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: "#600432ff" }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+
+  buttonInnerContainer: {
+    backgroundColor: "#72063C",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,66 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    backgroundColor: "#4e0228ff",
+    // flex: 1,
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.5,
+    alignItems: "center",
+    gap: 5,
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    paddingVertical: 2,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
+  buttonContainer: {
+    flex: 1,
+  },
+});
